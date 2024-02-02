81b4ceabe185ff9abb53ce35a0d78ff264d1b35d
Rahul Kumar <90387491+rkarahul@users.noreply.github.com>
2024-02-02 18:04:27 +0530

    Add files via upload
    complete this project

---
+++ Real_time_face_detection.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+
+yolo = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+
+with open("coco.names", "r") as file:
+    classes = [line.strip() for line in file.readlines()]
+layer_names = yolo.getLayerNames()
+output_layers = [layer_names[i - 1] for i in yolo.getUnconnectedOutLayers()]
+
+colorRed = (0,0,255)
+colorGreen = (0,255,0)
+
+#Loading Images
+vdo = cv2.VideoCapture(0)
+while True:
+    isimg,img = vdo.read()
+    img = cv2.resize(img,(1000,900))
+    if isimg == False:
+        break
+    height, width, channels = img.shape
+    
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    
+    yolo.setInput(blob)
+    outputs = yolo.forward(output_layers)
+    
+    class_ids = []
+    confidences = []
+    boxes = []
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+    
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+    
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+    
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0,255,0), 3)
+            cv2.putText(img, label, (x, y + 10), cv2.LINE_AA, 3, (0,0,255), 3)
+    
+    
+    cv2.imshow("Real_time_detection", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+                break
+    
+cv2.destroyAllWindows()
+vdo.release()
